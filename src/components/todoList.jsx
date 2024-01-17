d849a8a925a5e2e2e96d0fc70b8e4d00ecdb8a86
Giang <gianglt@gmail.com>
2024-01-17 16:47:06 +0700

    Filter List

--- src/components/todoList.jsx
+++ src/components/todoList.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import {
   atom,
   selector,
@@ -6,20 +7,19 @@ import {
   useRecoilState,
   useSetRecoilState,
 } from "recoil";
-import { todoListState, addItemSelector } from "../recoil/todoState";
-import { List, Page, Box, Button, Input, Icon, useNavigate } from "zmp-ui";
+import { todoListState, addTodoItemSelector, 
+  changeTodoItem,
+  toggleTodoItem, deleteTodoItem,
+  filteredTodoListState, 
+  todoListFilterState,
+  todoListStatsState
+ } from "../recoil/todoState";
+import { List, Page, Box, Button, Input, Icon, useNavigate, Checkbox } from "zmp-ui";
 
-// utility for creating unique Id
-let id = 1;
-
-function getId() {
-  return id++;
-}
 
 function TodoItemCreator() {
   const [inputValue, setInputValue] = useState("");
-  const setTodoList = useSetRecoilState(todoListState);
-  const addTodoList = useSetRecoilState(addItemSelector);
+  const addTodoList = useSetRecoilState(addTodoItemSelector);
 
   const addItem = () => {
     addTodoList(inputValue);
@@ -48,78 +48,109 @@ function TodoItemCreator() {
   );
 }
 
-function TodoItem({ item }) {
+function TodoItem({ idx, item }) {
   const [todoList, setTodoList] = useRecoilState(todoListState);
   const index = todoList.findIndex((listItem) => listItem === item);
 
-  const editItemText = ({ target: { value } }) => {
-    const newList = replaceItemAtIndex(todoList, index, {
-      ...item,
-      text: value,
-    });
+  const toggleItem = useSetRecoilState(toggleTodoItem);
+  const deleteItem = useSetRecoilState(deleteTodoItem);
+  const changeItem = useSetRecoilState(changeTodoItem);
 
-    setTodoList(newList);
+  //console.log('KEY', item.id, index, idx);
+
+  const changeItemRow = ({ target: { value }}) => {
+    var obj = {
+      index : index,
+      text: value
+    }
+    changeItem(obj);
   };
 
   const toggleItemCompletion = () => {
-    const newList = replaceItemAtIndex(todoList, index, {
-      ...item,
-      isComplete: !item.isComplete,
-    });
-
-    setTodoList(newList);
+    toggleItem(index);
   };
 
-  const deleteItem = () => {
-    const newList = removeItemAtIndex(todoList, index);
-
-    setTodoList(newList);
+  const deleteItemRow = () => {
+    deleteItem(index);
   };
+
 
   return (
-    <ContentWrapper px={4} py={8}>
+    <Box px={4} py={8}>
       <Box flex flexDirection="row" alignItems="center">
-        <Box ml={6}>{item.id}</Box>
-        <Box ml={6}>
-          <input type="text" value={item.text} onChange={editItemText} />
+        <Box ml={6} style={{flex:1}}>{item.id}</Box>
+        <Box ml={6} style={{flex:3}}>
+          <Input type="text" value={item.text} onChange={changeItemRow} />
         </Box>
-        <Box ml={6}>
-          <input
-            type="checkbox"
+        <Box ml={6} style={{flex:1, textAlign:'center'}}>
+
+          <Checkbox          
             checked={item.isComplete}
             onChange={toggleItemCompletion}
-          />
+        />
+
         </Box>
-        <Box ml={6}>
-          <button onClick={deleteItem}>X</button>
+        <Box ml={6} style={{flex:1}}>
+          <Button onClick={deleteItemRow}>X</Button>
         </Box>
       </Box>
-    </ContentWrapper>
+    </Box>
   );
 }
 
-function replaceItemAtIndex(arr, index, newValue) {
-  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+export function TodoListFilters() {
+  const [filter, setFilter] = useRecoilState(todoListFilterState);
+
+  const updateFilter = ({target: {value}}) => {
+    setFilter(value);
+  };
+
+  return (
+    <>
+      Filter:
+      <select value={filter} onChange={updateFilter}>
+        <option value="Show All">All</option>
+        <option value="Show Completed">Completed</option>
+        <option value="Show Uncompleted">Uncompleted</option>
+      </select>
+    </>
+  );
 }
 
-function removeItemAtIndex(arr, index) {
-  return [...arr.slice(0, index), ...arr.slice(index + 1)];
+function TodoListStats() {
+  const {
+    totalNum,
+    totalCompletedNum,
+    totalUncompletedNum,
+    percentCompleted,
+  } = useRecoilValue(todoListStatsState);
+
+  const formattedPercentCompleted = Math.round(percentCompleted);
+
+  return (
+    <ul>
+      <li>Total items: {totalNum}</li>
+      <li>Items completed: {totalCompletedNum}</li>
+      <li>Items not completed: {totalUncompletedNum}</li>
+      <li>Percent completed: {formattedPercentCompleted}</li>
+    </ul>
+  );
 }
 
 export function TodoList() {
-  const todoList = useRecoilValue(todoListState);
 
+  const todoList = useRecoilValue(filteredTodoListState);
   const showList = () => {
     console.log("LIST", todoList);
   };
   return (
     <>
-      {/* <TodoListStats /> */}
-      {/* <TodoListFilters /> */}
+      <TodoListStats />
+      <TodoListFilters />
       <TodoItemCreator />
 
       {todoList.map((todoItem) => (
-        <TodoItem key={todoItem.id} item={todoItem} />
+        <TodoItem key={todoItem.id} idx={todoItem.id} item={todoItem} />
       ))}
 
       <Button onClick={showList}>Show List</Button>
--- src/recoil/todoState.js
+++ src/recoil/todoState.js
@@ -1,18 +1,23 @@
-import { atom, selector,useRecoilState, useSetRecoilState } from "recoil";
+import { atom, selector, useRecoilState, useSetRecoilState } from "recoil";
+
+const initState = [
+  { id: 1, text: "Task 1", isComplete:false }, 
+  { id: 2, text: "Task 2", isComplete:false },
+];
 
 export const todoListState = atom({
     key: 'TodoList',
-    default: [],
+    default: initState,
   });
 
 
 // utility for creating unique Id
-let id = 1;
+let id = 3;
 function getId() {
   return id++;
 }
 
-export const addItemSelector = selector({
+export const addTodoItemSelector = selector({
     key: 'addItemSelector',
     get: ({get}) => get(todoListState),
     set: ({set, get}, inputValue) => {
@@ -29,29 +34,93 @@ export const addItemSelector = selector({
     },
   });
 
+export const changeTodoItem = selector({
+    key: 'changeTodoItem',
+    get: ({get}) => get(todoListState),
+    set: ({set, get}, obj) => {
+      const todoList = get(todoListState);
+      var item = todoList[obj.index];
+      const newTodoList = replaceItemAtIndex(todoList, obj.index, {
+      ...item,
+      text: obj.text,
+      });
+
+      set(todoListState, newTodoList);
+    },
+  });
+
+export const toggleTodoItem = selector({
+    key: 'toggleItemCompletion',
+    get: ({get}) => get(todoListState),
+    set: ({set, get}, index) => {
+      const todoList = get(todoListState);
+
+      var item = todoList[index];
+      const newTodoList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      isComplete: !item.isComplete,
+      });
+
+      set(todoListState, newTodoList);
+    },
+  });
+
+export const deleteTodoItem = selector({
+    key: 'deleteItem',
+    get: ({get}) => get(todoListState),
+    set: ({set, get}, index) => {
+      const todoList = get(todoListState);
+      const newTodoList = removeItemAtIndex(todoList, index);
+      set(todoListState, newTodoList);
+    },
+  });
 
 
-// export const toggleItemCompletion = (index) => {
-    
-
-//     const newList = replaceItemAtIndex(todoList, index, {
-//       ...item,
-//       isComplete: !item.isComplete,
-//     });
-
-//     setTodoList(newList);
-//   };
-
-// export  const deleteItem = (index) => {
-//     const newList = removeItemAtIndex(todoList, index);
-
-//     setTodoList(newList);
-//   };
-
-// function replaceItemAtIndex(arr, index, newValue) {
-//     return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
-// }
+function replaceItemAtIndex(arr, index, newValue) {
+    return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+}
   
-// function removeItemAtIndex(arr, index) {
-//     return [...arr.slice(0, index), ...arr.slice(index + 1)];
-// }  
+function removeItemAtIndex(arr, index) {
+    return [...arr.slice(0, index), ...arr.slice(index + 1)];
+}  
+
+export const todoListFilterState = atom({
+  key: 'TodoListFilter',
+  default: 'Show All',
+});
+
+export const filteredTodoListState = selector({
+  key: 'FilteredTodoList',
+  get: ({get}) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoListState);
+
+    switch (filter) {
+      case 'Show Completed':
+        return list.filter((item) => item.isComplete);
+      case 'Show Uncompleted':
+        return list.filter((item) => !item.isComplete);
+      default:
+        return list;
+    }
+  },
+});
+
+
+export const todoListStatsState = selector({
+  key: 'TodoListStats',
+  get: ({get}) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum * 100;
+
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted,
+    };
+  },
+});
